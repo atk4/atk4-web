3a69519bc847f2a50af076607ff86637239a4382
Romans Malinovskis <r@agiletech.ie>
2011-02-09 10:34:27 +0000

    fix link

--- page/doc.php
+++ page/doc.php
@@ -2,7 +2,7 @@
 class page_doc extends Page {
 	function init(){
 		parent::init();
-		$this->add('Button',null,'start')->set('Start Interactive Introduction')->js('click')->univ()->redirect($this->api->getDestinationURL('/doc/intro/1'));
+		$this->add('Button',null,'start')->set('Start Interactive Introduction')->js('click')->univ()->redirect($this->api->getDestinationURL('/doc/intro'));
 	}
 	function defaultTemplate(){
 		return array('page/doc');
