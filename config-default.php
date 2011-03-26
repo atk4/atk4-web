a380fc96fa7ee78e62d2a98bbc2038f7783a6465
Romans Malinovskis <r@agiletech.ie>
2011-03-26 08:01:05 +0200

    embedd orchestra's mysql info

--- config-default.php
+++ config-default.php
@@ -1,7 +1,8 @@
 <?php
 
 $config['atk']['base_path']='/atk4/';
-$config['dsn']='mysql://root:root@localhost/project';
+//$config['dsn']='mysql://root:root@localhost/project';
+$config['dsn']='mysql://user_2f3f53c8:D6ILbAg.0IAGZ4@10.194.111.8/db_2f3f53c8';
 
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
