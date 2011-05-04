09220f9d45dcda2568c517bb8efec398f3e049ed
Romans Malinovskis <r@agiletech.ie>
2011-05-04 22:27:48 +0100

    Theme improvements, new documentation structure design

---
+++ doc/dbupdates/ap.001.03.sql
@@ -0,0 +1,2 @@
+alter table filestore_file add sample_file enum('Y','N') default 'N';
+/* alter table filestore_image add sample_file enum('Y','N') default 'N'; */
--- page/download/success.php
+++ page/download/success.php
@@ -2,12 +2,14 @@
 class page_download_success extends Page {
 	function init(){
 		parent::init();
+		$this->add('Controller_JSProxy')->proxy('http://platform.twitter.com/widgets.js');
+		/*
 		$f=$this->add('Form',null,null,array('form_empty'));
 		//$f->js(true)->addClass('tweet_this_form');
 		$f->setFormClass('form-twitter');
 		$f->setLayout("download_success_form_layout");
 
-		$this->js(true)->univ()->setTimeout($this->js()->_enclose()->univ()->location('/distfiles/'.$_GET['file']),1000);
+	//	$this->js(true)->univ()->setTimeout($this->js()->_enclose()->univ()->location('/distfiles/'.$_GET['file']),1000);
 
 		$this->template->trySet('file',addslashes(htmlspecialchars($_GET['file'])));
 		$f->addField('text','t','Tweet This')
@@ -17,8 +19,19 @@ class page_download_success extends Page {
 			$f->js()->univ()->location('http://twitter.com?status='.rawurlencode($f->get('t')))->execute();
 			exit;
 		}
+		*/
 	}
 	function defaultTemplate(){
 		return array('page/download/success');
+	}
+}
+
+class Controller_JSProxy extends AbstractController {
+	function proxy($url){
+		$this->owner->js(true,'$.atk4.includeJS("'.$this->api->getDestinationURL(null,array($this->name=>1)).'",true);');
+		if(isset($_GET[$this->name])){
+			readfile($url);
+			exit;
+		}
 	}
 }
