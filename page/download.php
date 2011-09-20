6762ec39d5e68d34b9892eb83ac180df7e9a68d4
Romans Malinovskis <r@agiletech.ie>
2011-09-20 19:01:56 +0100

    add redirect from download page.

--- page/download.php
+++ page/download.php
@@ -3,6 +3,8 @@
 class page_download extends Page {
 	function init(){
 		parent::init();
+        $this->api->redirect('commercial/store');
+        // TODO: add this page back
 		for ($i = 1; $i <= 4; $i++){
 			$this->js('click')->_selector('#download'.$i)->univ()->frameURL('<i class="icon-arrow-big"></i>Downloading...',
                 $this->js()->_selectorThis()->attr('href'),array('customClass'=>'popup-download', 'width' => 500, 'resizable' => false, 'draggable' => false));
