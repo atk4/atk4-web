50b47641235ff9998a43d7c23f930568ee203dfd
Romans Malinovskis <r@agiletech.ie>
2011-09-21 11:08:32 +0100

    fix for custom selectors

--- lib/jMyUI.php
+++ lib/jMyUI.php
@@ -7,11 +7,11 @@ class jMyUI extends jUI {
                 foreach($chains as $chain){
                     $s=$chain->selector?$chain->selector:'#'.$chain->owner->name;
 
-                    $n=$chain->owner->short_name;
+                    if($chain->selector)$n=$chain->selector;else$n=$chain->owner->short_name;
                     if(!$n)$n=$chain->owner->name;
 
                     if($chain->owner instanceof Button){
-                        $nn=preg_replace('/[^a-zA-Z0-9]/','',$chain->owner->template->get('Content'));
+                        $nn=preg_replace('/[^a-zA-Z0-9=]/','',$chain->owner->template->get('Content'));
                         if($nn)$n=$nn;
                     }
 
