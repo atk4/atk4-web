8dc44416eb89a73cfdc75a39910c8fbc81aa149a
Romans Malinovskis <r@agiletech.ie>
2011-10-02 17:29:41 +0100

    re-add default content into screencasts

--- doc/dbupdates/atk4.04.006.sql
+++ doc/dbupdates/atk4.04.006.sql
@@ -18,3 +18,11 @@ ALTER TABLE `atk_video`
 ALTER TABLE `atk_video`
     ADD COLUMN `ord` INT(11) NULL DEFAULT NULL  AFTER `id` ;
 
+
+INSERT INTO `atk_video` (`id`, `ord`, `name`, `is_public`, `src`, `descr`, `type`, `atk_version`, `complexity`)
+VALUES
+	(1,3,'Introduction to Agile Toolkit (part 1)','Y','http://files.agiletech.ie/atk4-gita-1.mov','Brief introduction to Agile Toolkit by Romans and Gita.','screencast','4.1','Intro'),
+	(2,6,'Introduction to Pages and Multiple Pages','Y','http://files.agiletech.ie/atk4-gita-pages.mov','Introduction to how create linked pages such as those used with Multi-step wizard or multi-tabbed pages.','screencast','4.1','Intro'),
+	(3,7,'Project from Zero (Older screencast) Part 1','Y','http://files.agiletech.ie/atk4-zero-start/movie.f4v','Explains how Agile Toolkit execute. Introduces to routing, database use, Models and Controllers.','screencast','4.0','Learner'),
+	(5,8,'Project from Zero (Older screencast) Part 2','Y','http://files.agiletech.ie/atk4-zero-start/movie.f4v','Explains how Agile Toolkit execute. Introduces to routing, database use, Models and Controllers.','screencast','4.0','Learner');
+    
