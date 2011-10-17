cbbf722e4dc8d10de07f5506d83fdb345fa46ad7
Romans Malinovskis <r@agiletech.ie>
2011-10-18 00:09:00 +0100

    move account to rootdir

--- lib/AtkMenu.php
+++ lib/AtkMenu.php
@@ -42,6 +42,7 @@ class AtkMenu extends Menu {
 			case'develop': 
 				$this->api->template->trySet('menu_develop','class="current"');
 
+				$menu->addMenuItem('Account','community/account');
 				$menu->addMenuItem('Share Help','community/help');
 				$menu->addMenuItem('Share Code','community/code');
 				$menu->addMenuItem('Share Love','community/love');
@@ -54,7 +55,6 @@ class AtkMenu extends Menu {
 				$this->api->template->trySet('menu_services','class="current"');
 
 
-				$menu->addMenuItem('Account','commercial/account');
 				$menu->addMenuItem('Prices','commercial/store');
 				$menu->addMenuItem('Services','commercial/services');
 				//$menu->addMenuItem('Products','commercial/products');
